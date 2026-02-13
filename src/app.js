13acc78e14cc815a74911df2e22dd03795c3aee7
Mateo <mateo1maibach@gmail.com>
2026-02-12 21:36:28 -0300

    tests + ajustes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
-const express = require('express');
-const sequelize = require('./config/db'); 
-require('dotenv').config();
-const authRoutes = require('../src/routes/authRoutes');
-
+const express = require("express");
+const sequelize = require("./config/db");
+require("dotenv").config();
+const authRoutes = require("../src/routes/authRoutes");
 
 const app = express();
 app.use(express.json());
-app.use('/api/auth',authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-    try {
-        
-        await sequelize.authenticate();
-        console.log('Base de datos conectada.');
+  try {
+    await sequelize.authenticate();
+    console.log("Base de datos conectada.");
 
-        
-        app.listen(PORT, () => {
-            console.log(`Servidor corriendo en http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error('No se pudo iniciar el servidor:', error);
-    }
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("No se pudo iniciar el servidor:", error);
+  }
 }
 
 startServer();
